398b738ee14c438ecb488097f5ecd8af6b8ee353
S Cersofios <sofiacersofios@hotmail.com>
2024-02-06 18:39:02 -0300

    Pruebas unitarias

--- backend/db.go
+++ backend/db.go
@@ -10,9 +10,9 @@ import (
 // Db es una variable global que representa la conexión a la base de datos
 var Db *sql.DB
 
-func init() {
+func InitDB(dataSourceName string) {
 	var err error
-	Db, err = sql.Open("mysql", "root:root@tcp(172.23.0.2:3306)/mydatabase")
+	Db, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		log.Fatal(err)
 	}
--- backend/main.go
+++ backend/main.go
@@ -18,6 +18,7 @@ type Item struct {
 }
 
 func main() {
+	InitDB("root:root@tcp(172.23.0.2:3306)/mydatabase")
 	router := mux.NewRouter()
 	router.HandleFunc("/api/data", GetData).Methods("GET")
 	router.HandleFunc("/api/data", CreateData).Methods("POST")
